41efe48cd408ca94978a2e48e933a73e46173dbf
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-12 18:48:58 +0200

    Update CarRentIcon.jsx

--- src/images/CarRentIcon.jsx
+++ src/images/CarRentIcon.jsx
@@ -3,7 +3,7 @@ import icons from "./icons.svg";
 const SVGimage = ({ name }) => {
   return (
     <svg style={{ width: "20px", height: "20px" }}>
-      <use xlinkHref={`${icons}#${name}`} />
+      <use xlinkHref={`../images/icons.svg#${name}`} />
     </svg>
   );
 };
