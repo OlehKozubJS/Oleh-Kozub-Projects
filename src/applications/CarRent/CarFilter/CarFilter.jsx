8d6220544b52139ca2bc02947ffa13d71b731851
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-04 22:02:46 +0200

    Update CarFilter.jsx

--- src/applications/CarRent/CarFilter/CarFilter.jsx
+++ src/applications/CarRent/CarFilter/CarFilter.jsx
@@ -3,7 +3,11 @@ import { SVGimage } from "../images";
 import cars from "../advertsCars.json";
 
 const CarFilter = ({ carId, onClose }) => {
-  return <div></div>;
+  return (
+    <div>
+      <></>
+    </div>
+  );
 };
 
 export { CarFilter };
