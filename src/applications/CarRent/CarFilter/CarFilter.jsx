07af0446b06bd47da77f206827796310b6374f9e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-04 22:02:13 +0200

    Update CarFilter.jsx

--- src/applications/CarRent/CarFilter/CarFilter.jsx
+++ src/applications/CarRent/CarFilter/CarFilter.jsx
@@ -3,29 +3,6 @@ import { SVGimage } from "../images";
 import cars from "../advertsCars.json";
 
 const CarFilter = ({ carId, onClose }) => {
-  const [car, setCar] = useState({});
-
-  useEffect(() => {
-    const carsData = JSON.parse(cars);
-    console.log(carsData);
-    const carData = carsData.find((car) => car.id === carId);
-    setCar(carData);
-  }, []);
-
-  useEffect(() => {
-    const handleEsc = (event) => {
-      if (event.type === "keydown" && event.key === "Escape") {
-        onClose();
-      }
-    };
-
-    window.addEventListener("keydown", handleEsc);
-
-    return () => {
-      window.removeEventListener("keydown", handleEsc);
-    };
-  }, []);
-
   return <div></div>;
 };
 
