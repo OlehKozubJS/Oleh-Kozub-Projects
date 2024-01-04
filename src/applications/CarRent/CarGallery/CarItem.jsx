b8fb0a91489bdfa9bcf619e135bbcd2983813d8b
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-04 22:25:08 +0200

    Update CarItem.jsx

--- src/applications/CarRent/CarGallery/CarItem.jsx
+++ src/applications/CarRent/CarGallery/CarItem.jsx
@@ -2,31 +2,34 @@ import { useEffect, useState } from "react";
 import { cars } from "../../../db";
 
 const CarItem = ({ carId }) => {
-  const [{ make, model, year, rentalPrice, address, rentalCompany, type, id }] =
-    useState(
-      cars.find((car) => {
-        car.id === carId;
-      })
-    );
+  const [
+    { img, make, model, year, rentalPrice, address, rentalCompany, type, id },
+  ] = useState(
+    cars.find((car) => {
+      car.id === carId;
+    })
+  );
 
   return (
     <li>
       <div>
-        <img src="" alt="" />
+        <img src={img} alt="" />
       </div>
       <h3>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+        <span>
+          <span>{make}</span>
+          <span>{model}</span>
+          <span>{year}</span>
+        </span>
+        <span>{rentalPrice}</span>
       </h3>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>{address.split(", ")[1]}</li>
+        <li>{address.split(", ")[2]}</li>
+        <li>{rentalCompany}</li>
+        <li>{type}</li>
+        <li>{model}</li>
+        <li>{id}</li>
       </ul>
       <button>Learn More</button>
     </li>
