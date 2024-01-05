f9b3c89b3d4d214f7d8d2afccefe45c36099111d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-05 18:23:23 +0200

    Update Applications.jsx

--- src/Applications.jsx
+++ src/Applications.jsx
@@ -8,10 +8,28 @@ const CarRent = lazyImport("CarRent");
 const MyClock = lazyImport("MyClock");
 
 function Applications() {
+  const [app, setApp] = useState("CarRent");
+
+  const selectApp = (event) => {
+    setApp(event.target.dataset.value);
+  };
+
   return (
     <Suspense>
-      <CarRent />
-      <MyClock />
+      <header>
+        <nav>
+          <button type="button" onClick={selectApp} data-value="CarRent">
+            Car Rent
+          </button>
+          <button type="button" onClick={selectApp} data-value="MyClock">
+            My Clock
+          </button>
+        </nav>
+      </header>
+      <main>
+        {app === "CarRent" && <CarRent />}
+        {app === "MyClock" && <MyClock />}
+      </main>
     </Suspense>
   );
 }
