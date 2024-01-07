1f79fbdfd7a6788c244ddfeeb9d9de60a1eb0fbc
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-08 00:01:34 +0200

    Update sounds.js

--- src/applications/MusicTheory/sounds.js
+++ src/applications/MusicTheory/sounds.js
@@ -1,1 +1,28 @@
-const sounds = {};
+const sounds = [
+  { name: "До", degrees: 1, semitones: 1 },
+  { name: "Ре", degrees: 2, semitones: 3 },
+  { name: "Мі", degrees: 3, semitones: 5 },
+  { name: "Фа", degrees: 4, semitones: 6 },
+  { name: "Соль", degrees: 5, semitones: 8 },
+  { name: "Ля", degrees: 6, semitones: 10 },
+  { name: "Сі", degrees: 7, semitones: 12 },
+];
+
+const signs = [
+  { name: "-дубль-бемоль", semitones: -2 },
+  { name: "-бемоль", semitones: -1 },
+  { name: "", semitones: 0 },
+  { name: "-дієз", semitones: 1 },
+  { name: "-дубль-дієз", semitones: 2 },
+];
+
+const octaves = [
+  " субконтроктави",
+  " контроктави",
+  " великої октави",
+  " малої октави",
+  " пешої октави",
+  " другої октави",
+  " третьої октави",
+  " четвертої октави",
+];
