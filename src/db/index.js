04b681fb5e950cd35e0b3bfacf11c6fe5d24ccf8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-07 23:03:11 +0200

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,5 @@
 import cars from "./advertsCars.json";
 import makes from "./makes.json";
+import applications from "./applications.json";
 
-export { cars, makes };
+export { cars, makes, applications };
