71c3aa3a4ca42eb2be14dcda10f9823ea3de70d9
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-11 00:16:38 +0200

    Update getSoundAndSign.js

--- src/applications/MusicTheory/MusicTheoryFunctions/getSoundAndSign.js
+++ src/applications/MusicTheory/MusicTheoryFunctions/getSoundAndSign.js
@@ -23,6 +23,12 @@ const separateOctaves = ({ degrees, semitones }) => {
   return { degrees: clearDegrees, semitones: clearSemitones, octaves };
 };
 
-const getSoundAndSign = ({ degrees, semitones }) => {};
+const getSoundAndSign = (data) => {
+  const { degrees, semitones, octaves } = separateOctaves(data);
+  const sound = getSound(degrees);
+  const sign = getSign(semitones - soundSemitones);
+  const octave = getOctave(octaves);
+  return { name: sound.name, sign: sign.name, octave };
+};
 
 export { getSoundAndSign };
